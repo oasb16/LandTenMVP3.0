4a474b1864b642d321216164aa73d8c08b57e4a3
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-10-26 18:54:51 -0500

    manual fixes

---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,29 @@
+from . import (
+    chat,
+    incident,
+    job,
+    agent,
+    thread,
+    agent_summary,
+    media,
+    profile,
+    task,
+    chat_stream,
+    property,
+    contractor,
+)
+
+__all__ = [
+    "chat",
+    "incident",
+    "job",
+    "agent",
+    "thread",
+    "agent_summary",
+    "media",
+    "profile",
+    "task",
+    "chat_stream",
+    "property",
+    "contractor",
+]
